63734a466e9ec914b6f74bc9cec741bb84c30c13
Vicki-Alden <vic.alden23@gmail.com>
2024-08-01 17:08:57 -0700

    add components folder

---
+++ src/components/HomeMobile.tsx
@@ -0,0 +1,119 @@
+import Image from 'next/image';
+import { Container } from '@mui/material';
+import { Anton } from 'next/font/google';
+
+const anton = Anton({ subsets: ['latin'], weight: '400' });
+
+const styles = {
+  fontSize: '23px',
+  fontWeight: '300',
+};
+
+function Text({ str, size, opt }: { str: string; size?: string; opt?: {} }) {
+  return (
+    <p style={{ ...styles, ...opt, ...(size && { fontSize: size }) }}>{str}</p>
+  );
+}
+
+function StickyButton({
+  label,
+  handleOpenCalendar,
+}: {
+  label: string;
+  handleOpenCalendar?: () => void;
+}) {
+  return (
+    <button onClick={handleOpenCalendar} className="book-now-button-sticky">
+      {label}
+    </button>
+  );
+}
+
+export default function HomeMobile() {
+  const handleOpenCalendar = () => {
+    window.location.href = 'https://calendly.com/valden-wilshiregfs/30min';
+  };
+
+  return (
+    <Container style={{ maxWidth: '1200px', marginBottom: '100px' }}>
+      <p
+        className={`${anton.className}`}
+        style={{
+          fontSize: '32px',
+          marginBottom: '10px',
+        }}
+      >
+        All Day Services
+      </p>
+
+      <Image
+        src="/family-portrait.png"
+        alt="family"
+        width={800}
+        height={1000}
+        priority
+      />
+
+      <div style={{ textAlign: 'center', marginBottom: '30px' }}>
+        <p
+          className={`${anton.className}`}
+          style={{
+            fontSize: '65px',
+            lineHeight: '71px',
+            marginBottom: '20px',
+            letterSpacing: '3px',
+            marginTop: 13,
+          }}
+        >
+          Financial Services
+        </p>
+        <Text str={'Annuities'} />
+        <Text str={'Mortgage Protection'} />
+        <Text str={'Final Expense Insurance'} />
+        <Text str={'Life Insurance'} />
+        <Text str={'Disability Insurance'} />
+        <Text
+          str={'Critical Illness Insurance'}
+          opt={{ marginBottom: '10%' }}
+        />
+        <StickyButton
+          label="BOOK AN APPOINTMENT"
+          handleOpenCalendar={handleOpenCalendar}
+        />
+      </div>
+
+      <p
+        className={`${anton.className}`}
+        style={{
+          fontSize: '35px',
+          lineHeight: '60px',
+          marginBottom: '10px',
+          marginTop: 13,
+        }}
+      >
+        About Me
+      </p>
+
+      <Text
+        size="16px"
+        str={`My name is Victoria Alden and I specialize in helping individuals manage 
+            their financial risks, ensuring they are prepared 
+            for any uncertainties that life may bring. With a focus on 
+            creating secure and steady income streams, I aim to provide 
+            peace of mind and financial stability for my clients.
+            Whether it's through strategic planning or personalized 
+            advice, I am dedicated to guiding you towards a secure financial 
+            future.`}
+      />
+
+      <Image
+        src="/headshot-victoria.jpg"
+        alt="family"
+        width={200}
+        height={400}
+        priority
+        style={{ margin: 'auto', marginTop: '10px' }}
+      />
+    </Container>
+  );
+}
